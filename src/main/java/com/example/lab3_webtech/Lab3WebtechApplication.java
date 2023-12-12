f51af93b6a7ff1528cb17aa7bf1aa229156d5418
Samusenko_D <loneluna@mail.ru>
2023-12-12 07:44:27 +0300

    Initial commit

---
+++ src/main/java/com/example/lab3_webtech/Lab3WebtechApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab3_webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3WebtechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3WebtechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3_webtech/Lab3WebtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3WebtechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
